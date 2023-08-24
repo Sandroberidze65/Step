5185b530fb8324c977005ef2b9f4d1ea1f0e96e3
alexs <sandroberidze65@gmail.com>
2023-08-25 03:13:48 +0400

    added teams

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,14 +13,17 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
 
   register(){
     this.accountService.register(this.model).subscribe({
       next: () =>{
         this.cancel();
       },
-      error: error=> console.log(error)
+      error: error=> {
+        this.toastr.error(error.error),
+        console.log(error.error)
+      }
     })
   }
 
