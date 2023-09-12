a6fc5b4acdd09d35e3be9379d3de3505c75c95b7
alexs <sandroberidze65@gmail.com>
2023-09-12 21:51:53 +0400

    login filds cleaning

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -25,6 +25,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: _ => {
         this.router.navigateByUrl('/members');
+        this.model = {};
       }
     })
   }
